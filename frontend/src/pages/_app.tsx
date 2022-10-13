ee97ad4febda9e0fd626f4c5faf2d615e8354538
madawei2699 <madawei2699@gmail.com>
2022-10-13 16:58:46 +0800

    chore: use webrtc adapter.js to improve compatibility

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import React from "react"
 
 import { AppProps } from "next/app"
+import Script from "next/script"
+
 import "../styles/tailwind.scss"
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script src="https://cdnjs.cloudflare.com/ajax/libs/webrtc-adapter/8.1.2/adapter.js" />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
