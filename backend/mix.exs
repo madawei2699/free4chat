a10f10ebc58bc9760745933842e150a21c38560e
madawei2699 <madawei2699@gmail.com>
2022-10-13 16:28:56 +0800

    chore: try to upgrade elixir to v1.14.1

--- backend/mix.exs
+++ backend/mix.exs
@@ -5,7 +5,7 @@ defmodule Free4chat.MixProject do
     [
       app: :free4chat,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
