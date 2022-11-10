8b3fb185b3553a17fb8454bef7c380ecea8096a5
madawei2699 <madawei2699@gmail.com>
2022-11-10 08:07:32 +0800

    doc: dev notes, enable search

--- doc/dev-notes/src/.vuepress/config.ts
+++ doc/dev-notes/src/.vuepress/config.ts
@@ -1,4 +1,5 @@
 import { googleAnalyticsPlugin } from "@vuepress/plugin-google-analytics";
+import { searchPlugin } from '@vuepress/plugin-search'
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
@@ -18,9 +19,15 @@ export default defineUserConfig({
   shouldPrefetch: false,
 
   plugins: [
-    [
       googleAnalyticsPlugin({
         id: "G-8HBGR25L32",
       }),
-    ]]
+      searchPlugin({  
+        locales: {
+          "/": {
+            placeholder: "搜索文档",
+          },
+        },
+      }),
+    ]
 });
